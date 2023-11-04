178214259218d4362ee7b782fb5fe118b193daf0
techmonster <techmonster707@gmail.com>
2023-11-04 20:20:08 +0100

    First Local Commit

---
+++ src/main/java/africa/quickmenu/first_project/App.java
@@ -0,0 +1,13 @@
+package africa.quickmenu.first_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
